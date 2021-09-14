8613986d10285228d9b3fdd886d1c58068cc6e77
Felix Miranda <feedmite@hotmail.com>
2021-09-13 19:33:15 -0500

    fixin' carousel

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -5,15 +5,13 @@ const Inicio = () => {
   return (
     <>
       <section className="description-container">
-        <h2>Cartagena de Indias</h2>
+        <h2>Todos</h2>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
           temporibus nam fuga odit officiis corporis? Voluptatibus tempore quia
           repellendus cum enim dolorem fugiat, deserunt esse recusandae culpa
           totam cumque fuga. Lorem ipsum dolor sit amet consectetur, adipisicing
-          elit. Labore, reprehenderit quasi reiciendis rerum inventore assumenda
-          possimus commodi et nisi, libero, dolores quia fuga corrupti vitae
-          velit. Assumenda eos repudiandae in.
+          elit.
         </p>
       </section>
     </>
